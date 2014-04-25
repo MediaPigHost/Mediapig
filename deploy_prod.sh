4a74bf0b387e4abb72325348f460e2bd06a8a022
Palgie <shootex@gmail.com>
2014-04-25 20:58:09 +0100

    Added single quotes

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -4,4 +4,4 @@ set -e
 
 rsync -rPt --delete --exclude=.idea --exclude=.DS_Store ./ mediapig@live.mediapig.co.uk:/home/mediapig/public_html
 
-ssh mediapig@live.mediapig.co.uk node --harmony /home/mediapig/public_html/server.js &
+ssh mediapig@live.mediapig.co.uk 'node --harmony /home/mediapig/public_html/server.js &'
