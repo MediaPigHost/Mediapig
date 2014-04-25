0ca59bf396a58d55c35755802c6c2254a291dd75
Palgie <shootex@gmail.com>
2014-04-25 22:51:32 +0100

    Updated deploy script to service call

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -3,3 +3,5 @@
 set -e
 
 rsync -rPt --delete --exclude=.idea --exclude=.DS_Store ./ mediapig@live.mediapig.co.uk:/home/mediapig/public_html
+
+ssh mediapig@live.mediapig.co.uk 'sudo /etc/init.d/node-mpig restart'
