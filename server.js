5ca5fc7853c6ef0d662644e87c03f44a2e0a065a
Shahzain Badruddin <shpunjani@gmail.com>
2014-10-25 17:20:39 +0100

    enabling clustering and amazing shizzles

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var http                = require('http');
 var fs                  = require('fs');
 var app                 = express();
 var winston             = require('winston');
-var domain             = require('domain').create();
+var domain              = require('domain').create();
+var cluster             = require('cluster');
+var numCPUs             = require('os').cpus().length || 1;
 
 var logger = new (winston.Logger)({
     transports: [
@@ -18,71 +20,87 @@ var logger = new (winston.Logger)({
     ]
 });
 
-domain.on('error', function (err) {
-    console.log(err);
-    logger.log('error', err, function (err, level, msg, meta) {
-        process.exit(1);
-    });
-});
+if (cluster.isMaster) {
 
-domain.run(function(){
-    app.use(express.static(__dirname + '/public'));
-    app.use(bodyParser());
-    app.engine('html', cons.swig);
-
-// set .html as the default extension
-    app.set('view engine', 'html');
-    app.set('views', __dirname + '/src/templates');
-    app.use(cookieParser());
-    app.use(session({
-        secret: 'bacon wave',
-        cookie: {
-            maxAge: 60000
-        }
-    }));
-
-    httpRequestHelpers.SetRequests(app);
-
-    app.use(function(req, res, next){
-        console.log('oopsie?');
-        res.status(404);
-
-        // respond with html page
-        if (req.accepts('html')) {
-            res.render('404', { url: req.url });
-            return;
-        }
-
-        // respond with json
-        if (req.accepts('json')) {
-            res.send({ error: 'Not found' });
-            return;
-        }
-
-        // default to plain-text. send()
-        res.type('txt').send('Not found');
-    });
-
-    app.use(function(err, req, res, next){
-        console.log('ooh lala');
-        res.status(err.status || 500);
-        res.render('500', { error: err });
-    });
-
-    if (process.env.DEVENV === 'false') {
-
-        var options = {
-            key: fs.readFileSync('/etc/ssl/server.key'),
-            cert: fs.readFileSync('/etc/ssl/bundle.crt')
-        };
-
-        // Create an HTTPS service identical to the HTTP service.
-        https.createServer(options, app).listen(4333);
-    }
-    else {
-        // Create an HTTP service.
-        http.createServer(app).listen(4333);
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
 
-    console.log('Listening on port 4333');
-});
+    cluster.on('exit', function(worker, code, signal) {
+        winston.log('info', 'worker ' + worker.process.pid + ' died');
+        cluster.fork();
+    });
+}
+else {
+
+    domain.on('error', function (err) {
+        console.log(err);
+        logger.log('error', err, function (err, level, msg, meta) {
+            process.exit(1);
+        });
+    });
+
+    domain.run(function(){
+
+        app.set('port', 4333);
+        app.use(express.static(__dirname + '/public'));
+        app.use(bodyParser());
+        app.engine('html', cons.swig);
+
+        // set .html as the default extension
+        app.set('view engine', 'html');
+        app.set('views', __dirname + '/src/templates');
+        app.use(cookieParser());
+        app.use(session({
+            secret: 'bacon wave',
+            cookie: {
+                maxAge: 60000
+            }
+        }));
+
+        httpRequestHelpers.SetRequests(app);
+
+        app.use(function(req, res, next){
+            console.log('oopsie?');
+            res.status(404);
+
+            // respond with html page
+            if (req.accepts('html')) {
+                res.render('404', { url: req.url });
+                return;
+            }
+
+            // respond with json
+            if (req.accepts('json')) {
+                res.send({ error: 'Not found' });
+                return;
+            }
+
+            // default to plain-text. send()
+            res.type('txt').send('Not found');
+        });
+
+        app.use(function(err, req, res, next){
+            console.log('ooh lala');
+            res.status(err.status || 500);
+            res.render('500', { error: err });
+        });
+
+        if (process.env.DEVENV === 'false') {
+
+            var options = {
+                key: fs.readFileSync('/etc/ssl/server.key'),
+                cert: fs.readFileSync('/etc/ssl/bundle.crt')
+            };
+
+            // Create an HTTPS service identical to the HTTP service.
+            https.createServer(options, app).listen(app.get('port'));
+        }
+        else {
+            // Create an HTTP service.
+            http.createServer(app).listen(app.get('port'));
+        }
+
+        console.log('Listening on port ', app.get('port'));
+    });
+}
