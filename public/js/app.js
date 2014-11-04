d7dcf804d7bba86a4a78864085bb47d09f00b5fb
Palgie <shootex@gmail.com>
2014-11-04 21:45:53 +0000

    More animationaes

--- public/js/app.js
+++ public/js/app.js
@@ -41,13 +41,22 @@ curl([
                 }
 
                 window.onscroll = function (event) {
-                  console.log(animateStatus[0].active);
                   if (window.pageYOffset > 1000 && (animateStatus[0].active != 'true' || typeof animateStatus[0].active === 'undefined')) {
                     var el = document.getElementsByClassName('oneclick-window');
                     if (el) {
                       el[0].className += ' animate';
                     }
                     animateStatus[0].active = 'true';
+                    setTimeout(function () {
+
+                      app.help.addEventListenerByClass('oneclick-window', 'mouseover', function(e){
+                        e.target.parentNode.className += ' animate-hover'
+                      });
+
+                      app.help.addEventListenerByClass('oneclick-window', 'mouseout', function(e){
+                        app.help.removeClass(el[0], 'animate-hover');
+                      });
+                    }, 2000);
                   }
                 }
 
