8812f78a661e5d19a0be3c93352080c18dc538af
Aleksi Tappura <Aleksi@Aleksi-Air.Elisa>
2014-11-09 00:05:40 +0200

    Add modal base

--- public/js/account.js
+++ public/js/account.js
@@ -22,4 +22,17 @@ $(document).ready(function(){
       $('#panel').remove();
   });
 
+  var modal = $('.modal');
+  $(".btn").on("click", function() {
+    $(modal).toggleClass('modal--show');
+  });
+
+  $(".overlay").on("click", function() {
+    $(modal).toggleClass('modal--show');
+  });
+
+  $(".modal__close").on("click", function() {
+    $(modal).toggleClass('modal--show');
+  });
+
 });
