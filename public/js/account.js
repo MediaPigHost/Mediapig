aac22189ba218633cb6024de0dca431a2821843f
Aleksi Tappura <Aleksi@Aleksi-Air.Elisa>
2014-11-09 11:04:48 +0200

    Add and style

--- public/js/account.js
+++ public/js/account.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  /* Custom select box */
   $("select").each(function(){
     $(this).wrap('<div class="selectbox"/>');
     $(this).after("<span class='selecttext'></span><span class='select-arrow'></span>");
@@ -18,21 +19,37 @@ $(document).ready(function(){
         }
   });
 
+  /* Dismiss notification */
   $("#remove").click(function(){
       $('#panel').remove();
   });
 
-  var modal = $('.modal');
-  $(".btn").on("click", function() {
-    $(modal).toggleClass('modal--show');
+  /* Payment method modal */
+  var modal = $('.method-modal');
+  $(".method-btn").on("click", function() {
+    $(modal).toggleClass('method-modal--show');
   });
 
-  $(".overlay").on("click", function() {
-    $(modal).toggleClass('modal--show');
+  $(".method-overlay").on("click", function() {
+    $(modal).toggleClass('method-modal--show');
   });
 
-  $(".modal__close").on("click", function() {
-    $(modal).toggleClass('modal--show');
+  $(".method-modal__close").on("click", function() {
+    $(modal).toggleClass('method-modal--show');
+  });
+
+  /* Upgrade modal */
+  var modal2 = $('.upgrade-modal');
+  $(".upgrade-btn").on("click", function() {
+    $(modal2).toggleClass('upgrade-modal--show');
+  });
+
+  $(".upgrade-overlay").on("click", function() {
+    $(modal2).toggleClass('upgrade-modal--show');
+  });
+
+  $(".upgrade-modal__close").on("click", function() {
+    $(modal2).toggleClass('upgrade-modal--show');
   });
 
 });
