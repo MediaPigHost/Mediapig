cf881fab4b2319807d936ee3a7e50f677d415e4c
Palgie <shootex@gmail.com>
2014-11-15 13:00:45 +0000

    Never enough DOM selectors!

--- public/js/order.js
+++ public/js/order.js
@@ -17,18 +17,44 @@ define(['require', 'exports', 'module', 'helpers'], function (require, exports, 
 
       document.getElementById('order-total-value').innerHTML = (parseFloat(price)).toFixed(2);
     },
+    refreshSelectionViews : function(name, id, value){
+      var dropdownSelectedEl = document.getElementsByClassName('option-' + name),
+          dropdownTriggerEl = dropdownSelectedEl[0].getElementsByClassName('option-trigger'),
+          optionSectionEl = document.getElementsByClassName('section-' + name),
+          optionEl = optionSectionEl[0].querySelectorAll('[data-product-id="'+id+'"]'),
+          optionElSiblings = optionSectionEl[0].getElementsByClassName('order-grid-item'),
+          dropdownListEl = dropdownSelectedEl[0].getElementsByClassName('option-dropdown'),
+          dropdownEl = dropdownListEl[0].querySelectorAll('[data-product-id="'+id+'"] a'),
+          dropdownElValue = dropdownEl[0].getElementsByClassName('option-value'),
+          dropdownElSiblings = dropdownEl[0].parentNode.parentNode.getElementsByTagName('li');
+
+          // Updating dropdown value
+          dropdownTriggerEl[0].getElementsByClassName('option-value')[0].innerHTML = value;
+
+          // Remove all dropdown active classes
+          for (var i = 0; i < dropdownElSiblings.length; i++) {
+            helpers.removeClass(dropdownElSiblings[i].getElementsByClassName('option-link')[0], "enabled");
+          }
+          // Add new dropdown active class
+          dropdownEl[0].className += " enabled";
+
+          // Remove all dropdown active classes
+          for (var i = 0; i < optionElSiblings.length; i++) {
+            helpers.removeClass(optionElSiblings[i], "selected");
+          }
+
+          optionEl[0].className += " selected";
+
+    },
     events : function(){
       helpers.addEventListenerByClass('order-grid-item', 'click', function (e) {
 
           var target = e.currentTarget,
-              selectionParent = target.parentNode;
-              siblings = selectionParent.getElementsByClassName('order-box');
+              sectionName = target.getAttribute('data-name'),
+              targetId = target.getAttribute('data-product-id'),
+              targetValue = target.getAttribute('data-value');
 
-          for (var i = 0, length = siblings.length; i < length; i++) {
-            helpers.removeClass(siblings[i].parentNode, "selected");
-          }
-          target.className += " selected";
-
+          order.refreshSelectionViews(sectionName, targetId, targetValue);
           order.calculatePrice();
 
           e.preventDefault();
@@ -38,12 +64,12 @@ define(['require', 'exports', 'module', 'helpers'], function (require, exports, 
       helpers.addEventListenerByClass('option-trigger', 'click', function (e) {
 
           var target = e.currentTarget,
-              selectionParent = target.parentNode;
-              siblings = selectionParent.parentNode.getElementsByClassName('option'),
+              targetParent = target.parentNode;
+              siblings = targetParent.parentNode.getElementsByClassName('option'),
               isSelected = false;
 
-          if (selectionParent.className.match(/\bselected\b/)) {
-              helpers.removeClass(selectionParent, "selected");
+          if (targetParent.className.match(/\bselected\b/)) {
+              helpers.removeClass(targetParent, "selected");
               isSelected = true;
           }
 
@@ -52,13 +78,28 @@ define(['require', 'exports', 'module', 'helpers'], function (require, exports, 
           }
 
           if (!isSelected) {
-              selectionParent.className += " selected";
+              targetParent.className += " selected";
           }
 
           isSelected = false;
 
           e.preventDefault();
       });
+
+      helpers.addEventListenerByClass('option-link', 'click', function (e) {
+
+          var target = e.currentTarget,
+              targetParent = target.parentNode,
+              sectionName = targetParent.getAttribute('data-name'),
+              targetId = targetParent.getAttribute('data-product-id'),
+              targetValue = targetParent.getAttribute('data-value');
+
+          order.refreshSelectionViews(sectionName, targetId, targetValue);
+          order.calculatePrice();
+
+          e.preventDefault();
+      });
+
 
     }
   }
