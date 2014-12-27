82c388fa79cbb356c99f3ae28a58624754642fcd
Palgie <shootex@gmail.com>
2014-12-27 22:18:46 +0000

    Changed stage to staging

--- deploy_stage.sh
+++ deploy_stage.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-rsync -rPt --delete --exclude=.idea --exclude=.DS_Store ./ mediapig@stage.mediapig.co.uk:/home/mediapig/node
+rsync -rPt --delete --exclude=.idea --exclude=.DS_Store ./ mediapig@staging.mediapig.co.uk:/home/mediapig/node
 
-ssh mediapig@stage.mediapig.co.uk 'sudo /etc/init.d/node-mpig restart'
+ssh mediapig@staging.mediapig.co.uk 'sudo /etc/init.d/node-mpig restart'
