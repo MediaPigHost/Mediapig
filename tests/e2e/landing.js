e5f7ba9112979d47caea6f0db186a57c6888070f
Palgie <shootex@gmail.com>
2014-12-29 00:45:02 +0000

    Removing tests

--- tests/e2e/landing.js
+++ tests/e2e/landing.js
@@ -1,13 +1,10 @@
-var data = require('../../src/content/site.json');
+var casper = require('casper').create();
 
-casper.test.begin("Landing Page Loads", 2, function(test) {
-
-  casper.start('http:' + data.url + '/', function() {
-    test.assertTitle("Mediapig | Hosting without the hassle", "Mediapig homepage title is the one expected");
-    test.assertExists('.body .btn', "main call to action is found");
+  casper.start('http://locahost:4333/', function() {
+    this.test.assertTitle("Mediapig | Hosting without the hassle", "Mediapig homepage title is the one expected");
+    this.test.assertExists('.body .btn', "main call to action is found");
   });
 
   casper.run(function() {
-    test.done();
+    this.test.done(2); // checks that 5 assertions have been executed
   });
-});
